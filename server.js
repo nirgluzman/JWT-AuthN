facc753371ac4eca04f97245bac1ba1ac3b5fb70
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 12:45:52 +0100

    add login for password check

--- server.js
+++ server.js
@@ -25,17 +25,40 @@ app.get("/users", (req, res) => {
 app.post("/users", async (req, res) => {
   try {
     // create password salting.
-    const salt = await bcrypt.genSalt();
+    //const salt = await bcrypt.genSalt();
 
     // bcrypt prepends the salt to hashedPassword (adds on at the beginning).
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    console.log(salt);
+    // bcrypt to salt and password hash in one command.
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    //console.log(salt);
     console.log(hashedPassword);
 
     const user = { name: req.body.name, password: hashedPassword };
     users.push(user);
     res.status(201).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  // check if user in database.
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("cannot find user");
+  }
+
+  try {
+    // compare plain text password and hashed password.
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("success");
+    } else {
+      res.send("not allowed");
+    }
   } catch (err) {
     console.log(err);
     res.status(500).send();
