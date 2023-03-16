29b29581fd0cff08c7a12e2eee37b9d517720e6e
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 12:13:39 +0100

    save name/password in plain text

--- server.js
+++ server.js
@@ -5,12 +5,25 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3000;
 
+// Users database - for testing.
+const users = [];
+
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to AuthN Tutorial !");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.post("/users", (req, res) => {
+  const user = { name: req.body.name, password: req.body.password };
+  users.push(user);
+  res.status(201).send();
 });
 
 app.listen(PORT, () => {
