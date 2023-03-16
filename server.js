9682d9e5581ccc6fbae4df1a61c73a71104c3c8c
nirgluzman <nir.gluzman@gmail.com>
2023-03-16 12:31:56 +0100

    add bcrypt for salting and password hashing

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const express = require("express");
 
+const bcrypt = require("bcrypt");
+
 const PORT = process.env.PORT || 3000;
 
 // Users database - for testing.
@@ -20,10 +22,24 @@ app.get("/users", (req, res) => {
   res.json(users);
 });
 
-app.post("/users", (req, res) => {
-  const user = { name: req.body.name, password: req.body.password };
-  users.push(user);
-  res.status(201).send();
+app.post("/users", async (req, res) => {
+  try {
+    // create password salting.
+    const salt = await bcrypt.genSalt();
+
+    // bcrypt prepends the salt to hashedPassword (adds on at the beginning).
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    console.log(salt);
+    console.log(hashedPassword);
+
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
 });
 
 app.listen(PORT, () => {
